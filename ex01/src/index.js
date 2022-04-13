4cdcdd5b07acac34d3db9845932d2429bc9f8621
AnelB <anel.b@live.com>
2022-04-13 20:56:46 +0200

    prvi up

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+// only change code below this line
+const array1 = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
+
+function main(arr) {
+	let array2 = [...arr];
+	return array2;
+}
+console.log(main(array1));
+module.exports = main;
---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+const users = [
+    { firstName: "John", lastName: "Doe", age: 24, gender: "male" },
+    { firstName: "Jane", lastName: "Doe", age: 5, gender: "female" },
+    { firstName: "Jim", lastName: "Carrey", age: 54, gender: "male" },
+    { firstName: "Kate", lastName: "Winslet", age: 40, gender: "female" }
+];
+
+
+
+
+
